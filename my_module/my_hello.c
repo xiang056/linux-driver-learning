f2f0f572746bb19cfeb72911206913cef69f5a04
Edward <congxiang56@gmail.com>
2026-06-24 22:32:00 +0800

    Add my_module: hello world kernel module written from scratch

---
+++ my_module/my_hello.c
@@ -0,0 +1,20 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Edward");
+MODULE_DESCRIPTION("say hello");
+
+static int __init hello_init(void){
+	printk(KERN_INFO "Hello Kernel！\n");
+	return 0;
+}
+
+static void __exit hello_exit(void){
+	printk(KERN_INFO "Goodbye, Kerenl~\n");
+}
+
+module_init(hello_init);
+module_exit(hello_exit);
+
+
